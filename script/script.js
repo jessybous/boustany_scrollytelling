5c32ae90879383f2603504053a4ab46c212760ad
Kye Moon <jessyboustany2013@gmail.com>
2023-04-27 19:21:09 -0400

    2.2a

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+
+
 const play = document.querySelector('.scrollicon');
 
   gsap.from('.scrollicon', { 
@@ -5,3 +7,16 @@ const play = document.querySelector('.scrollicon');
     y: '-20',
     yoyo:true,
   });
+
+  let delay;
+  let sprite = document.querySelector(".ougabouga");
+  let body = document.querySelector("body");
+  document.body.classList.remove("is-scrolling");
+
+window.addEventListener("scroll", (delay)=>{
+  clearTimeout(delay);
+  body.classList.add("is-scrolling");
+  delay = setTimeout(function(){
+    body.classList.remove("is-scrolling");
+  }, 1000)
+})
