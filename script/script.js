b9cbd0ebbd224e76cdc6af0f1137b8d7d37a732f
Jessica Boustany <jessyboustany2013@gmail.com>
2023-04-28 00:41:51 -0400

    now i can kill myself

--- script/script.js
+++ script/script.js
@@ -20,3 +20,19 @@ window.addEventListener("scroll", (delay)=>{
     body.classList.remove("is-scrolling");
   }, 1000)
 })
+
+
+let chapitredeux = gsap.timeline();
+chapitredeux.fromTo(".chien-tombe", { x: "-100vh"}, {y: "100vh", duration: 4,repeat:-1 })
+
+let chapitretrois = gsap.timeline();
+chapitretrois.fromTo(".chien-3", {x:"-100vw"}, {x: "100vw", duration: 6})
+
+let chapitrequatre = gsap.timeline()
+chapitretrois.fromTo(".dog-water", {x:"-100vw", y: "100vh"}, {x:"100vw", y:"-100vh",  duration: 7, })
+
+let chapitresix = gsap.timeline();
+chapitresix.fromTo(".chienfin", {x:"-100vh"}, {x:"10vw",  duration: 3})
+.to(".chienfin", {y: "-40vh", rotation:-65, duration: 3})
+let chapitrecinq = gsap.timeline();
+chapitrecinq.fromTo(".chien", {x:"-100vw"}, {x:"100vw",   duration: 7})
