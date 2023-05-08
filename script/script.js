34eaef663c51bd8e18734ca9c409a90805dbf15c
Kye Moon <jessyboustany2013@gmail.com>
2023-05-08 18:08:19 -0400

    *COMMITS SUICIDE*

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 
+gsap.registerPlugin(ScrollTrigger);
 
 const play = document.querySelector('.scrollicon');
 
@@ -22,17 +23,76 @@ window.addEventListener("scroll", (delay)=>{
 })
 
 
-let chapitredeux = gsap.timeline();
-chapitredeux.fromTo(".chien-tombe", { x: "-35vw", y: "-80vh"}, {y: "100vh", duration: 4,repeat:-1 })
+let chapitredeux = gsap.timeline({
+  scrollTrigger: {
+    markers: true,
+    start: "center bottom",
+    end: "bottom top",
+    toggleActions: "restart complete reset",
+    trigger: ".chien-tombe"
+  }
+})
+chapitredeux.fromTo(".chien-tombe", { x: "-35vw", y: "-50vh"}, {y: "100vh", duration: 4,repeat:-1, 
+})
 
-let chapitretrois = gsap.timeline();
+let chapitretrois = gsap.timeline({
+  scrollTrigger: {
+
+    markers: true,
+    start: "center bottom",
+    end: "bottom top",
+    toggleActions: "restart complete reset",
+    trigger: ".chien-3"
+  }
+});
 chapitretrois.fromTo(".chien-3", {x:"-100vw"}, {x: "100vw", duration: 6})
 
-let chapitrequatre = gsap.timeline()
-chapitretrois.fromTo(".dog-water", {x:"-100vw", y: "100vh"}, {x:"100vw", y:"-100vh",  duration: 7, })
+let chapitrequatre = gsap.timeline({
+  scrollTrigger: {
 
-let chapitresix = gsap.timeline();
+    markers: true,
+    start: "center bottom",
+    end: "bottom top",
+    toggleActions: "restart complete reset",
+    trigger: ".dog-water"
+  }
+})
+chapitrequatre.fromTo(".dog-water", {x:"-100vw", y: "100vh"}, {x:"100vw", y:"-100vh",  duration: 7, })
+
+let chapitresix = gsap.timeline({
+  scrollTrigger: {
+
+    markers: true,
+    start: "center bottom",
+    end: "bottom top",
+    toggleActions: "restart complete reset",
+    trigger: ".chienfin"
+  }
+});
 chapitresix.fromTo(".chienfin", {x:"-100vh"}, {x:"10vw",  duration: 3})
 .to(".chienfin", {y: "-40vh", rotation:-65, duration: 3})
-let chapitrecinq = gsap.timeline();
-chapitrecinq.fromTo(".chien", {x:"-100vw"}, {x:"100vw",   duration: 7})
+
+
+let chapitrecinq = gsap.timeline({
+  scrollTrigger: {
+
+    markers: true,
+    start: "center bottom",
+    end: "bottom top",
+    toggleActions: "restart complete reset",
+    trigger: ".chien"
+  }
+});
+chapitrecinq.fromTo(".chien", {x:"-100vw"}, {x:"100vw",  duration: 7})
+
+let chapitreun = gsap.timeline({
+  scrollTrigger: {
+
+    markers: true,
+    start: "center bottom",
+    end: "bottom top",
+    toggleActions: "restart complete reset",
+    trigger: ".chien_5"
+  }
+});
+chapitreun.fromTo(".chien_5", {x:"-100vw"}, {x:"100vw",  duration: 7})
